05c4c8bfe8bce10400da29bd43ac6f7470587461
Filipe Esperandio <filipesperandio@gmail.com>
2020-03-04 16:59:05 -0300

    Move fixtures

--- src/__mocks__/ApiClient.ts
+++ src/__mocks__/ApiClient.ts
@@ -2,7 +2,7 @@ import * as fs from "fs"
 
 export class ApiClient {
   get(path: string, _params?: object): Promise<object> {
-    let raw = fs.readFileSync(`src/__tests__/fixtures/${path}.json`)
+    let raw = fs.readFileSync(`src/__fixtures__/${path}.json`)
     return Promise.resolve(JSON.parse(raw.toString()))
   }
 }
